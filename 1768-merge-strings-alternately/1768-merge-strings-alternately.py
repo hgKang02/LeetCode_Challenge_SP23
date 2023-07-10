ac4671151ce461a6f1534dcb8b04b2c85779aa2d
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2023-07-10 08:50:24 -0500

    Time: 44 ms (76.39%), Space: 16.4 MB (37.84%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,14 +1,33 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        word = ""
-        length = max(len(word1),len(word2))
+        first = True
+        dif = abs(len(word1) - len(word2))
+        if len(word1) - len(word2) < 0:
+            first = False
+        s = ""
+        if first == True:
+            for i in range(len(word1) - dif, len(word1)):
+                s += word1[i]
+        else:
+            for i in range(len(word2) - dif, len(word2)):
+                s += word2[i]
+        result = ""
+        for i in range(min(len(word1), len(word2))):
+            result += word1[i]
+            result += word2[i]
         
-        for i in range(length):
-            if i < len(word1):
-                word += word1[i]
-            if i < len(word2):
-                word += word2[i]
+        return result + s
+#         word = ""
+#         length = max(len(word1),len(word2))
         
+#         for i in range(length):
+#             if i < len(word1):
+#                 word += word1[i]
+#             if i < len(word2):
+#                 word += word2[i]
+                
+                
+        #
         # first = False
         # if len(word1) >= len(word2):
         #     length = len(word2)
@@ -24,5 +43,5 @@ class Solution:
         # else:
         #     for i in range(length, len(word2), 1):
         #         word += word2[i]
-        return word
+        # return word
         
