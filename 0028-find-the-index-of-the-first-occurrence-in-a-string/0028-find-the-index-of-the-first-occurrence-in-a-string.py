2b835bc2f66f91e2d539c0d89a3482f1bdcb9397
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2023-07-10 09:50:05 -0500

    Time: 53 ms (16.92%), Space: 16.4 MB (32.90%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,16 +3,16 @@ class Solution:
         end_point = len(haystack) - len(needle) + 1
         
         
-        # for i in range(end_point):
-        #     for j in range(len(needle)):
-        #         if haystack[i + j] != needle[j]:
-        #             break
-        #         elif j == len(needle) - 1:
-        #             return i
         for i in range(end_point):
             for j in range(len(needle)):
-                if haystack[i: i + j + 1] == needle:
+                if haystack[i + j] != needle[j]:
+                    break
+                elif j == len(needle) - 1:
                     return i
+        # for i in range(end_point):
+        #     for j in range(len(needle)):
+        #         if haystack[i: i + j + 1] == needle:
+        #             return i
 
         return -1
             
