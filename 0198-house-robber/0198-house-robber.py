78204c3ffd8d8464213c82debbdfa92f1fe41968
hgKang02 <68604896+hgkang-tech@users.noreply.github.com>
2023-07-22 03:07:37 -0500

    Time: 48 ms (57.98%), Space: 16.2 MB (75.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -9,7 +9,14 @@ class Solution:
             dp[0] = nums[0]
             dp[1] = nums[1]
             dp[2] = max(nums[2] + dp[0], dp[1])
-        
+            # if len(nums) % 2 == 0:
+            #     dp[2] = max(nums[2] + dp[0], dp[1])
+            #     for i in range(3, len(nums)):
+            #         dp[i] = max(nums[i] + dp[i - 2], dp[i -1], dp[ i - 3] + nums[i])
+            # else:
+            #     for i in range(2, len(nums)):
+            #         dp[i] = max(nums[i] + dp[i - 2], dp[i -1])
+            # dp[2] = max(nums[2] + dp[0], dp[1])
             for i in range(3, len(nums)):
                 dp[i] = max(nums[i] + dp[i - 2], dp[i -1], dp[ i - 3] + nums[i])
             return dp[len(dp) - 1]
